7ecf5761c9dc95649f2e4ce528409d704530cc69
Manlexey <Alexey_3588>
2020-08-14 14:05:11 +0300

    3.3. Разбор первой программы. [#342444]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Alexey Maneshin");
+        System.out.println("03.05.1988");
+
+
+    }
+}
