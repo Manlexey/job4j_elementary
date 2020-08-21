689ae93cd7f8f4da678f7ec87d2a036ad8a9db1a
Manlexey <Alexey_3588>
2020-08-21 17:12:36 +0300

    Converter

---
+++ src/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,5 @@
+package ru.job4j.converter;
+
+public class ConverterTest {
+
+}
---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+
+
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenConvert180RblThen3Dlr() {
+        int in = 180;
+        int expected = 3;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenConvert450EuroThen51Lira() {
+        int in = 450;
+        double expected = 8.7;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+}
