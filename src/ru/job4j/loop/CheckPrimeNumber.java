98647da123c583731d762100954ae180fe5b8f84
Manlexey <Alexey_3588>
2020-09-11 19:48:46 +0300

    Простые числа

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,10 +2,9 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = false;
-        for (int i = 2; i <= number; i++) {
-            prime = true;
-            if ( (number%i==0 && number !=i)) {
+        boolean prime =  number > 1;
+        for (int i = 2; i < number; i++) {
+            if ( (number%i==0)) {
                 prime = false;
                 break;
             }
