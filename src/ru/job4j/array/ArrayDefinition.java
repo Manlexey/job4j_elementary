1f556ef1a5e7e4418ba335c8719c245d8de29e05
Manlexey <Alexey_3588>
2020-09-15 23:46:11 +0300

    Заполнение массива

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,16 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short [] ages = new short[10];
-        System.out.println("Ages is" + " "+ ages.length);
-        String [] surnames = new String [100500];
-        System.out.println("surnames" + " " + surnames.length);
-        float [] prices = new float[40];
-        System.out.println("prices" + " " + prices.length);
+        //short [] ages = new short[10];
+       // System.out.println("Ages is" + " "+ ages.length);
+        String [] surnames = new String [5];
+        surnames [0] = "a";
+        surnames [1] = "b";
+        surnames [2] = "c";
+        surnames [3] = "d";
+        surnames [4] = "e";
+        System.out.println(surnames[4]);
+        //float [] prices = new float[40];
+        //System.out.println("prices" + " " + prices.length);
     }
 }
