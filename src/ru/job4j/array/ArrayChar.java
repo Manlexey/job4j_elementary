34c7e2da62cc448e6b5ce99fddd15c71a9943217
Manlexey <Alexey_3588>
2020-10-01 19:36:15 +0300

    Слово начинается с ...

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,10 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int x=0; x< word.length;x++) {
+        }if(word[1]!= pref[1]) {
+        }return result;
+    }
+}
