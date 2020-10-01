ab094b026d6792b8fff1c5d6272b15f68ce3b654
Manlexey <Alexey_3588>
2020-10-01 19:49:09 +0300

    Слово заканчивается на ...

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,10 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        if (word.length-1 == post.length-1) {
+            /* проверить. что массив word имеет последние элементы одинаковые с post */
+        }return result;
+    }
+}
