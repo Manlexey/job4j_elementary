c7853245b165e9dee613bb756dcbcb2d4c6c7b78
Manlexey <Alexey_3588>
2020-10-13 07:48:03 +0300

    6.6.4. Размер элемента двухмерного массива.

---
+++ src/ru/job4j/array/Matrix.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class Matrix {
+    public static int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        
+        return table;
+    }
+}
